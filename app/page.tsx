e6e8e19ec026eb8e3dbe5fd9ae54f1d054f733dd
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-03 14:19:26 +0530

    First call to OpenAI Chat using a client component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+"use client";
 import styles from "./page.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useEffect, useState } from "react";
+import { OpenAIApi, Configuration } from "openai";
+import * as dotenv from "dotenv";
 
 export default function Home() {
-  return <div className={styles.main}></div>;
+  const [test, setTest] = useState("test");
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+  console.log(process.env.OPENAI_API_KEY);
+
+  const openai = new OpenAIApi(configuration);
+
+  useEffect(() => {
+    async function fetchCompletion() {
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: "Hello world" }],
+      });
+      console.log(completion.data.choices[0].message);
+    }
+    fetchCompletion();
+  }, [test]);
+
+  return <div></div>;
 }
